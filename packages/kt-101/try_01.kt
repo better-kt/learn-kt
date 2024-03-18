39e43f993728522e5c43052b3f9d1526495ef7dd
Henry.Huang <hhglory@outlook.com>
2024-03-18 21:58:16 +0800

    init

---
+++ packages/kt-101/try_01.kt
@@ -0,0 +1,9 @@
+
+fun main() {
+    val one = 1 // Int
+    val threeBillion = 3000000000 // Long
+    val oneLong = 1L // Long
+    val oneByte: Byte = 1
+
+    println("$one $threeBillion $oneLong $oneByte")
+}
